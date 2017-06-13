0eee0f99d46187e60695f4fcb57558ecb04da77f
KeKsBoTer <simon.niedermayr@gmx.de>
2017-06-13 22:59:30 +0200

    - TwoThreeTree     - Renamed file to tree.go     - Added tree_test.go to test tree

--- src/trees/main.go
+++ src/trees/main.go
@@ -1,29 +1,5 @@
 package main
 
-import (
-	"trees/twoThreeTree"
-	"fmt"
-	"math/rand"
-)
-
-func main() {
-	/*m := map[string]interface{}{
-		"Simon": 1,
-		"Daniel": "test",
-		"Lücie": 564,
-		"Gustav": 1339,
-		"Heinz": 123,
-		"Dieter": 34,
-		"Caro": 21,
-		"Patrick": 123,
-	}*/
-	tree := twoThreeTree.NewTree()
-	values := make([]int,200)
-	for  i:=0;i<200;i++{
-		ran := rand.Intn(500)-100
-		values[i]=ran
-		tree.Insert(i)
-	}
-	fmt.Println(tree)
-	fmt.Printf("%d, %d",len(values),tree.Length())
+func main()  {
+	
 }
---
+++ src/trees/twoThreeTree/tree_test.go
@@ -0,0 +1,58 @@
+package twoThreeTree
+
+import (
+	"testing"
+	"math/rand"
+)
+
+func randTree(n int) ([]int, *tree) {
+	tree := NewTree()
+	values := make([]int, 200)
+	for i := 0; i < 200; i++ {
+		values[i] = rand.Intn(500) - 250
+	}
+
+	for i := range values {
+		tree.Insert(values[i])
+	}
+	return values, tree
+}
+
+func TestTree_Length(t *testing.T) {
+	values,tree := randTree(200)
+	RemoveDuplicates(&values)
+	tLen, vLen := tree.Length(), len(values)
+	if tLen != vLen {
+		t.Error(
+			"For", tree,
+			"expected", vLen,
+			"got", tLen,
+		)
+	}
+}
+
+func TestTree_Insert(t *testing.T) {
+	values,tree := randTree(200)
+	for i := range values {
+		if !tree.Member(values[i]) {
+			t.Error(
+				"For", values[i],
+				"expected", true,
+				"got", false,
+			)
+		}
+	}
+}
+
+func RemoveDuplicates(xs *[]int) {
+	found := make(map[int]bool)
+	j := 0
+	for i, x := range *xs {
+		if !found[x] {
+			found[x] = true
+			(*xs)[j] = (*xs)[i]
+			j++
+		}
+	}
+	*xs = (*xs)[:j]
+}
